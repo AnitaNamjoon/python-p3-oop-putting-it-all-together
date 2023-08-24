aef9014ac225faf9e02c3719c949b7337c3c6606
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-08-25 02:03:11 +0300

    Update shoe.py

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, price):
+        self.brand = brand
+        self._size = size
+        self._price = price
+        self._condition = "New"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, new_size):
+        if not isinstance(new_size, int):
+            raise TypeError("size must be an integer")
+        self._size = new_size
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price < 0:
+            raise ValueError("Price cannot be negative")
+        self._price = new_price
+
+    def repair(self):
+        self._condition = "New"
+        print("The shoe has been repaired.")
